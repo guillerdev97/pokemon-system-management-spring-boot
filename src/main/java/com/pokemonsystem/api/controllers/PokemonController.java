1c66a58de054ffa6ef9c7603792386cf19be3e38
Guillermo García <guillermieres97@gmail.com>
2022-11-21 17:00:46 +0100

    create first method pokemon controller

--- src/main/java/com/pokemonsystem/api/controllers/PokemonController.java
+++ src/main/java/com/pokemonsystem/api/controllers/PokemonController.java
@@ -1,9 +1,22 @@
 package com.pokemonsystem.api.controllers;
 
+import com.pokemonsystem.api.models.PokemonEntity;
+import org.apache.tomcat.jni.Proc;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/")
 public class PokemonController {
+    public ResponseEntity<List<PokemonEntity>> getPokemons() {
+        List<PokemonEntity> pokemons = new ArrayList<>();
+        pokemons.add(new PokemonEntity(1, "Squirtle", "Water"));
+        pokemons.add(new PokemonEntity(2, "Pikachu", "Electric"));
+        pokemons.add(new PokemonEntity(3, "Charmander", "Fire"));
+        return ResponseEntity.ok(pokemons);
+    }
 }

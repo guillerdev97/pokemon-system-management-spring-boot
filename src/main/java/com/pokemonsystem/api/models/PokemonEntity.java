3ca9cf4f3e2fdb7a188e4fcbee899557af9c0783
Guillermo García <guillermieres97@gmail.com>
2022-11-22 00:32:09 +0100

    modify spring data config and pokemon model

--- src/main/java/com/pokemonsystem/api/models/PokemonEntity.java
+++ src/main/java/com/pokemonsystem/api/models/PokemonEntity.java
@@ -9,14 +9,38 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+// lombok generates for us
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 public class PokemonEntity {
+    /* models are nouns or
+    pojos -> plain old java objects -> properties, getters and setters */
+
+    // models represent database tables
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // private variables and getters and setters = encapsulation
     private int id;
     private String name;
     private String type;
+
+    // getter
+   /* public int getId() {
+        return id;
+    }*/
+
+    // setter
+   /* public void setId(int id) {
+        this.id = id;
+    }*/
+
+    // constructor
+  /*  public PokemonEntity(int id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }*/
 }

376a0bfe3de514f3f751aadc718df60e45e7fde7
Guillermo García <guillermieres97@gmail.com>
2022-11-21 16:53:15 +0100

    add annotations to pokemon and review models

--- src/main/java/com/pokemonsystem/api/models/ReviewEntity.java
+++ src/main/java/com/pokemonsystem/api/models/ReviewEntity.java
@@ -1,9 +1,21 @@
 package com.pokemonsystem.api.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class ReviewEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String title;
     private String content;
